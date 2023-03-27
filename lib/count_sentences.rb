0af7b533988a66ab440bdf2e954a7c97b371c957
Eddington Rowellis <eddingtonibi@gmail.com>
2023-03-27 10:00:54 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
-
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
-
+  
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    # Split the string on any periods, question marks, or exclamation marks
+    # Note that the square brackets are used to match any one of the characters inside
+    # The + after the brackets means to match one or more of those characters in a row
+    sentences = self.split(/[.?!]+/)
+    
+    # Count the number of sentences
+    sentences.length
   end
 end
